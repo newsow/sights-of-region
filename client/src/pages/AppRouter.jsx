f657ba47aac65b2c479dc5360724be72a737faab
newsow <yegor.shkil@mail.ru>
2023-12-03 17:48:29 +0300

    add sights search functional

--- client/src/pages/AppRouter.jsx
+++ client/src/pages/AppRouter.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route,Routes } from 'react-router-dom'
 import AdminLogin from './AdminLogin'
+import Sights from './Sights'
 const AppRouter = () => {
   return (
     <div>
       <Routes>
         <Route path='/admin/login' element={<AdminLogin/>}/>
+        <Route path='/' element={<Sights/>}/>
       </Routes>
     </div>
   )
